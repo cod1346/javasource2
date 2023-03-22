f7e68e109b0cd06b646f5e846c088b4e9c96262c
cms <cod1346@naver.com>
2023-03-22 18:00:43 +0900

    SimpleDateFormatEx1 update

--- ch7/src/lang/SimpleDateFormatEx1.java
+++ ch7/src/lang/SimpleDateFormatEx1.java
@@ -9,7 +9,7 @@ public class SimpleDateFormatEx1 {
 	public static void main(String[] args) {
 		//시스템 날짜와 시간 가져오기
 		Date date = new Date();
-		
+		//날짜와 시간을 특정 형식에 맞춰 출력 가능하게 하는 클래스
 		SimpleDateFormat sdf = new SimpleDateFormat();
 		
 		System.out.println(date);
